42c8a9a2dddb4d367d4a4a08d18da16be15e3dc3
imranhAltersense <114974565+imranhAltersense@users.noreply.github.com>
2023-04-27 18:46:24 +0600

    pagination added

--- graphql/resolvers/author/fields.js
+++ graphql/resolvers/author/fields.js
@@ -2,15 +2,12 @@ import { Publisher, WorksAt } from "../../../db/models";
 
 const authorFields = {
   Author: {
-    publishers: async (author) => {
+    publishers: async (author, _, { loaders }) => {
       const worksAt = await WorksAt.find({ author: author.id });
 
-      const publishers = await Publisher.find({
-        _id: { $in: worksAt.map(({ publisher }) => publisher) },
-      });
-
-      return publishers;
+      return loaders.publisher.many(worksAt.map(({ publisher }) => publisher));
     },
   },
 };
+
 export default authorFields;
--- graphql/resolvers/author/queries.js
+++ graphql/resolvers/author/queries.js
@@ -1,9 +1,30 @@
 import { Author } from "../../../db/models";
 
 const authorQueries = {
-  authors: async (_, args, { loaders }) => {
-    const authors = await Author.find();
-    return loaders.author.many(authors.map(({ id }) => id));
+  authors: async (_, { params = { page: 1, pageSize: 2 } }, { loaders }) => {
+    const { page, pageSize } = params;
+    return {
+      results: async () => {
+        const authors = await Author.find()
+          .skip(pageSize * (page - 1))
+          .limit(pageSize);
+        return loaders.author.many(authors.map(({ id }) => id));
+      },
+      info: async () => {
+        const count = await Author.countDocuments();
+
+        const pages = Math.ceil(count / pageSize);
+        const prev = page > 1 ? page - 1 : null;
+        const next = page < pages ? page + 1 : null;
+
+        return {
+          count,
+          pages,
+          prev,
+          next,
+        };
+      },
+    };
   },
   author: async (_, { id }, { loaders }) => {
     // const author = await Author.findById(id);
